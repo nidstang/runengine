b37652b7573f065993da082722562481d5f1cb40
Pablo <pfranco@outlook.com>
2019-08-26 21:22:58 +0200

    first version and tests

---
+++ src/index.js
@@ -0,0 +1,327 @@
+export const Vec3 = (x, y, z) => ({
+    coords: { x, y, z },
+    multiply(scalar) {
+        this.coords.x = this.coords.x * scalar;
+        this.coords.y = this.coords.y * scalar;
+        this.coords.z = this.coords.z * scalar;
+
+        return this;
+    },
+
+    translateX(destiny) {
+        this.coords.x = destiny;
+    },
+
+    translateY(destiny) {
+        this.coords.y = destiny;
+    },
+
+    translateZ(destiny) {
+        this.coords.z = destiny;
+    },
+
+    set(coords) {
+        this.coords = coords;
+    },
+});
+
+const TransformProto = {
+    position: Vec3(0, 0, 0),
+    size: Vec3(0, 0),
+};
+
+export const withTransform = () => ({
+    transform: Object.create(TransformProto),
+    getTransform() {
+        const { x = 0, y = 0, z = 0 } = this.transform.position.coords;
+        const { x: w = 0, y: h = 0 } = this.transform.size.coords;
+        return { x, y, z, w, h };
+    },
+
+    setTransform({ x = 0, y = 0, z = 0, w = 0, h = 0 }) {
+        this.transform.position = Vec3(x, y, z);
+        this.transform.size = Vec3(w, h);
+        return this;
+    },
+
+    mapTransform(fn) {
+        return fn(this.getTransform());
+    },
+});
+
+const CreateFactory = proto => {
+    let instance = null;
+
+    return {
+        getInstance() {
+            if (!instance) {
+                instance = Object.create(proto);
+            }
+
+            return instance;
+        },
+    };
+};
+
+const ResourcesLoaderProto = {
+    resources: {},
+
+    load(pairs) {
+        return new Promise((resolve, reject) => {
+            let total = pairs.length;
+
+            pairs.forEach(([id, url]) => {
+                const image = new Image();
+                image.src = url;
+                image.onload = function onload() {
+                    total -= 1;
+                    if (total === 0) resolve();
+                };
+                image.onerror = function onerror(ev) {
+                    reject(ev);
+                };
+
+                this.resources[id] = image;
+            });
+        });
+    },
+
+    get(id) {
+        return this.resources[id];
+    },
+};
+
+export const ResourceLoader = CreateFactory(ResourcesLoaderProto);
+
+const SpriteProto = {
+    setImage(img) {
+        this.image = img;
+    },
+};
+
+export const Sprite = options => (
+    Object.assign(Object.create(SpriteProto), withTransform(), options)
+);
+
+export const Rect = options => Object.assign(withTransform(), options);
+
+export const Viewport = options => Object.assign(withTransform(), options);
+
+const GraphicsProto = {
+    ctx: null,
+
+    setContext(context) {
+        this.ctx = context;
+    },
+
+    clear() {
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    },
+
+    drawSprite(sprite) {
+        if (this.ctx) {
+            const { x, y, w, h } = sprite.getTransform();
+            this.ctx.drawImage(sprite.image, 0, 0, w, h, x, y, w, h);
+        }
+    },
+
+    drawRect(rect, color = '#000') {
+        const { x, y, w, h } = rect.getTransform();
+        this.ctx.save();
+        this.ctx.fillStyle = color;
+        this.ctx.fillRect(x, y, w, h);
+        this.ctx.restore();
+    },
+
+    getSprite(textureId) {
+        return Sprite({ image: ResourceLoader.getInstance().get(textureId) });
+    },
+};
+
+export const Graphics = CreateFactory(GraphicsProto);
+
+const CameraProto = {
+    viewport: Viewport(),
+    setViewport(viewport) {
+        this.viewport = viewport;
+        return this;
+    },
+
+    followEntity(entity) {
+        const { x: ex, y: ey, w: ew, h: eh } = entity.getTransform();
+        const { w, h } = this.viewport.getTransform();
+        this.viewport.setTransform({
+            x: ex + ew / 2 - w / 2,
+            y: ey + eh / 2 - h / 2,
+            w,
+            h,
+        });
+        return this;
+    },
+
+    mapTransformInViewport({ x, y, z, w, h }) {
+        const { x: vx, y: vy } = this.viewport.getTransform();
+        return { x: x - vx, y: y - vy, z, w, h };
+    },
+};
+
+export const Camera = CreateFactory(CameraProto);
+
+const EntityProto = {
+    components: [],
+    addComponent(component) {
+        this.components = [
+            ...this.components,
+            Object.assign(component, { entity: this }),
+        ];
+        return this;
+    },
+
+    update() {
+        this.components.map(component => component.update());
+    },
+};
+
+export const Entity = options => (
+    Object.assign(Object.create(EntityProto), withTransform(), options)
+);
+
+const ComponentProto = {
+    entity: Entity(),
+    graphics: Graphics.getInstance(),
+
+    update: () => true,
+};
+
+export const Component = options => Object.assign(Object.create(ComponentProto), options);
+
+const SceneProto = {
+    entities: [],
+
+    addEntity(entity) {
+        this.entities = [...this.entities, entity];
+    },
+
+    update() {
+        this.entities.map(entity => entity.update());
+    },
+};
+
+export const Scene = options => Object.assign(Object.create(SceneProto), options);
+
+// / End of engine
+
+// / Example
+
+/* const CameraLookat = Component({
+    update() {
+        Camera.getInstance().followEntity(this.entity);
+    },
+});
+
+const RectRenderer = Component({
+    init() {
+        const { x, y } = this.entity.getTransform();
+        this.rect = Rect().setTransform({ x, y, w: 10, h: 10 });
+        Graphics.getInstance().drawRect(this.rect);
+        return this;
+    },
+    update() {
+        this.rect.setTransform(
+            Camera.getInstance().mapTransformInViewport(this.entity.getTransform()),
+        );
+
+        Graphics.getInstance().drawRect(this.rect, this.color);
+    },
+});
+
+Camera.getInstance().setViewport(Viewport().setTransform({ w: 100, h: 100 }));
+const rectRenderer1 = Object.create(RectRenderer);
+const rectRenderer2 = Object.assign(Object.create(RectRenderer), {
+    color: '#539865',
+});
+const PlayerEntity = Entity({
+    moveEst() {
+        const { x, y, w, h } = this.getTransform();
+        this.setTransform({ x: x + 10, y, w, h });
+    },
+
+    moveWest() {
+        const { x, y, w, h } = this.getTransform();
+        this.setTransform({ x: x - 10, y, w, h });
+    },
+
+    moveNorth() {
+        const { x, y, w, h } = this.getTransform();
+        this.setTransform({ x, y: y - 10, w, h });
+    },
+
+    moveSouth() {
+        const { x, y, w, h } = this.getTransform();
+        this.setTransform({ x, y: y + 10, w, h });
+    },
+}).setTransform({ x: 10, y: 10, w: 10, h: 10 });
+
+PlayerEntity.addComponent(CameraLookat).addComponent(rectRenderer1);
+const TreeEntity = Entity()
+    .setTransform({ x: 20, y: 20, w: 10, h: 10 })
+    .addComponent(rectRenderer2);
+
+const MainScene = Scene({
+    entities: [TreeEntity, PlayerEntity],
+});
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+Graphics.getInstance().setContext(ctx);
+rectRenderer1.init();
+rectRenderer2.init();
+
+const animate = () => {
+    Graphics.getInstance().clear();
+    MainScene.update();
+    requestAnimationFrame(animate);
+};
+
+animate();
+
+document.getElementById('north').addEventListener('click', () => {
+    PlayerEntity.moveNorth();
+});
+
+document.getElementById('south').addEventListener('click', () => {
+    PlayerEntity.moveSouth();
+});
+
+document.getElementById('west').addEventListener('click', () => {
+    PlayerEntity.moveWest();
+});
+
+document.getElementById('est').addEventListener('click', () => {
+    PlayerEntity.moveEst();
+});
+
+/* const Renderer = Component({
+    setSprite: function(sprite) {
+      this.sprite = sprite;
+      return this;
+    },
+
+    setTexture: function(textureId) {
+      this.sprite = Graphics.getInstance().getSprite(textureId);
+      this.sprite.setPosition(this.entity.position);
+      Graphics.getInstance().drawSprite(this.sprite);
+      return this;
+    },
+
+    update: function() {
+      if (this.sprite) {
+        const entityPos = this.entity.position;
+        this.sprite.setPosition(
+          Camera.getInstance().getPositionInViewport(entityPos)
+        );
+      }
+
+      return this;
+    }
+  }); */
---
+++ tests/index.spec.js
@@ -0,0 +1,455 @@
+/* eslint-disable */
+import "@babel/polyfill";
+
+import {
+    Entity,
+    Component,
+    Sprite,
+    Scene,
+    Graphics,
+    Camera,
+    Viewport,
+    ResourceLoader,
+    Vec3,
+    Rect,
+    withTransform,
+} from '../src';
+
+const contextMock = {
+    save: jest.fn(),
+    restore: jest.fn(),
+    drawImage: jest.fn(),
+    fillRect: jest.fn(),
+    clearRect: jest.fn(),
+    canvas: {
+        width: 100,
+        height: 100,
+    },
+};
+
+Graphics.getInstance().setContext(contextMock);
+
+beforeEach(() => {
+    contextMock.save.mockClear();
+    contextMock.restore.mockClear();
+    contextMock.fillRect.mockClear();
+});
+
+describe('Tests for Api', () => {
+    describe('Vec3 tests', () => {
+        test('Vec3 must return an object with 3 coords, x, y and z', () => {
+            const vec3 = Vec3(2, 3, 3);
+            expect(vec3.coords).toEqual({ x: 2, y: 3, z: 3 });
+        });
+
+        test('Vec3 must allow to multiply by scalar', () => {
+            const vec3 = Vec3(2, 2, 2).multiply(2);
+
+            expect(vec3.coords).toEqual({ x: 4, y: 4, z: 4 });
+        });
+
+        test('Vec3 must allow to translate its coords to any destination', () => {
+            const vec3 = Vec3(1, 2, 3);
+            vec3.translateX(4);
+            vec3.translateY(4);
+            vec3.translateZ(4);
+
+            expect(vec3.coords).toEqual({ x: 4, y: 4, z: 4 });
+        });
+    });
+
+    describe('Transform tests', () => {
+        test('a transform must map another transfor into it', () => {
+            const rect = Object.assign({}, withTransform()).setTransform({
+                x: 1,
+                y: 1,
+                z: 2,
+                w: 1,
+                h: 1,
+            });
+            const newTransform = rect.mapTransform(({ x, y, ...rest }) => ({
+                ...rest,
+                x: x + 2,
+                y: y + 2,
+            }));
+
+            rect.setTransform(newTransform);
+
+            expect(rect.getTransform()).toEqual({ x: 3, y: 3, z: 2, w: 1, h: 1 });
+        });
+    });
+
+    describe('Rect tests', () => {
+        test('Rect factory must return a valid transform object', () => {
+            const rect = Rect();
+
+            expect(rect.getTransform()).toEqual({ x: 0, y: 0, z: 0, w: 0, h: 0 });
+        });
+
+        test('Rect.setPosition ought to update its transform', () => {
+            const rect = Rect().setTransform({ x: 1, y: 1, z: 1, w: 1, h: 1 });
+            expect(rect.getTransform()).toEqual({ x: 1, y: 1, z: 1, w: 1, h: 1 });
+        });
+    });
+
+    describe('Sprite tests', () => {
+        test('Sprite must get a transform', () => {
+            const sprite = Sprite();
+
+            expect(sprite.getTransform()).toEqual({ x: 0, y: 0, z: 0, w: 0, h: 0 });
+        });
+
+        test('Sprite must set the transform', () => {
+            const sprite = Sprite().setTransform({ x: 1, y: 2, z: 3, w: 10, h: 10 });
+
+            expect(sprite.getTransform()).toEqual({ x: 1, y: 2, z: 3, w: 10, h: 10 });
+        });
+
+        test('Sprite transform must be instance-safe', () => {
+            const s1 = Sprite();
+            const s2 = Sprite();
+            s2.setTransform({ x: 1, y: 2, z: 3, w: 10, h: 10 });
+
+            expect(s2.getTransform()).toEqual({ x: 1, y: 2, z: 3, w: 10, h: 10 });
+            expect(s1.getTransform()).toEqual({ x: 0, y: 0, z: 0, w: 0, h: 0 });
+        });
+    });
+
+    describe('Graphics tests', () => {
+        test('Graphics.getSprite must return a Sprite instance with the textureId passed', () => {
+            const image = new Image();
+            ResourceLoader.getInstance().resources.testTexture = image;
+
+            const sprite = Graphics.getInstance().getSprite('testTexture');
+
+            expect(sprite).not.toBeUndefined();
+            expect(sprite.image).toEqual(image);
+
+            ResourceLoader.getInstance().resources = {};
+        });
+
+        test('Graphics.drawRect must call to the context api with coords and color', () => {
+            const rect = Rect().setTransform(2, 2, 0, 10, 10);
+            Graphics.getInstance().drawRect(rect, '#fff');
+            const { x, y, w, h } = rect.getTransform();
+
+            expect(contextMock.save).toHaveBeenCalled();
+            expect(contextMock.fillRect).toHaveBeenCalledWith(x, y, w, h);
+            expect(contextMock.restore).toHaveBeenCalled();
+            expect(contextMock.fillStyle).toBe('#fff');
+        });
+
+        test('Graphics.clear must call to the context api to clear the canvas', () => {
+            Graphics.getInstance().clear();
+            expect(contextMock.clearRect).toHaveBeenCalledWith(0, 0, 100, 100);
+        });
+
+        test('Graphics.drawSprite must call to the context api to draw a Sprite image in its poisiton', () => {
+            const sprite = Sprite({ image: new Image() }).setTransform({
+                x: 2,
+                y: 3,
+                w: 10,
+                h: 10,
+            });
+            Graphics.getInstance().drawSprite(sprite);
+
+            expect(contextMock.drawImage).toHaveBeenCalledWith(
+                sprite.image,
+                0,
+                0,
+                10,
+                10,
+                2,
+                3,
+                10,
+                10,
+            );
+        });
+    });
+
+    describe('Component tests', () => {
+        test('Component factory with no params must return a valid compoonent', () => {
+            const component = Component();
+
+            expect(component.entity.getTransform()).toEqual({
+                x: 0,
+                y: 0,
+                z: 0,
+                w: 0,
+                h: 0,
+            });
+            expect(typeof component.update).toBe('function');
+            expect(component.graphics.ctx).toEqual(contextMock);
+        });
+
+        test('Component update must change its entity', () => {
+            const entity = Entity();
+            const comp = Component({
+                entity,
+                update() {
+                    this.entity.setTransform({ x: 1, y: 1, z: 1 });
+                },
+            });
+
+            comp.update();
+
+            expect(entity.getTransform()).toEqual({ x: 1, y: 1, z: 1, h: 0, w: 0 });
+        });
+
+        test('if we set a context in Graphics, all components must have that context', () => {
+            const mockContext = { drawImage: () => null };
+            Graphics.getInstance().setContext(mockContext);
+
+            const comp = Component();
+
+            expect(comp.graphics.ctx).toEqual(mockContext);
+        });
+    });
+
+    describe('Entity tests', () => {
+        test('Entity calls by no params must have default values', () => {
+            const e = Entity();
+            expect(e.getTransform()).toEqual({
+                x: 0,
+                y: 0,
+                z: 0,
+                w: 0,
+                h: 0,
+            });
+        });
+
+        test('Entity must be createdd correctly by its factory', () => {
+            const entity = Entity();
+
+            expect(entity.update).not.toBeUndefined();
+            expect(entity.getTransform()).toEqual({
+                x: 0,
+                y: 0,
+                z: 0,
+                w: 0,
+                h: 0,
+            });
+        });
+
+        test('Entity must be instance safe', () => {
+            const e1 = Entity().setTransform({ x: 1, y: 2, z: 3 });
+            const e2 = Entity().setTransform({ x: 3, y: 2, z: 1 });
+
+            expect(e2.getTransform().x).toBe(3);
+            expect(e1.getTransform().x).toBe(1);
+        });
+
+        test('Entity setPosition must return this context', () => {
+            const entity = Entity();
+
+            const ctx = entity.setTransform(0, 0, 0);
+
+            expect(ctx).toEqual(entity);
+        });
+
+        test('Entity must have a component collection that will be empty by default', () => {
+            const entity = Entity();
+            expect(entity.components).toEqual([]);
+        });
+
+        test('Entity addComponent must add a new component object to components collection', () => {
+            const entity = Entity();
+            const comp = Component();
+            entity.addComponent(comp);
+
+            expect(entity.components[0]).toEqual(comp);
+        });
+
+        test('Entity addComponent must pass entity context through the component', () => {
+            const entity = Entity({ position: Vec3(1, 2, 3) });
+            const comp = Component();
+            entity.addComponent(comp);
+
+            expect(entity.components[0].entity.position).toEqual(entity.position);
+        });
+
+        test('Entity update must call to every component update', () => {
+            const entity = Entity();
+            const comp = Component({
+                update() {
+                    this.entity.setTransform({ x: 1, y: 1, z: 1 });
+                },
+            });
+
+            entity.addComponent(comp);
+            entity.update();
+
+            expect(entity.getTransform()).toEqual({
+                x: 1,
+                y: 1,
+                z: 1,
+                h: 0,
+                w: 0,
+            });
+        });
+    });
+
+    describe('Scene tests', () => {
+        test('A scene basic must be created when we invoke Scene factory', () => {
+            const scene = Scene();
+
+            expect(scene.entities).toEqual([]);
+        });
+
+        test('The scene must allow to add entities to it', () => {
+            const scene = Scene();
+            const e1 = Entity();
+            const e2 = Entity();
+
+            scene.addEntity(e1);
+            scene.addEntity(e2);
+
+            expect(scene.entities[0]).toEqual(e1);
+            expect(scene.entities[1]).toEqual(e2);
+        });
+
+        test('Entities must be able to update from a scene', () => {
+            const doublePositionComponent = Object.assign(Component(), {
+                update() {
+                    this.entity.transform.position = this.entity.transform.position.multiply(
+                        2,
+                    );
+                },
+            });
+            const scene = Scene();
+            const e1 = Entity().setTransform({ x: 1, y: 1, z: 1 });
+
+            e1.addComponent(doublePositionComponent);
+            const e2 = Entity();
+
+            scene.addEntity(e1);
+            scene.addEntity(e2);
+
+            scene.update();
+
+            expect(e1.getTransform()).toEqual({ x: 2, y: 2, z: 2, w: 0, h: 0 });
+        });
+
+        test('Scene must render all entities in viewport coords', () => {});
+    });
+
+    describe('Viewport tests', () => {
+        test('Viewport without params must return a instance of viewport', () => {
+            const viewport = Viewport();
+
+            expect(viewport.getTransform()).toEqual({ x: 0, y: 0, z: 0, w: 0, h: 0 });
+        });
+
+        test('Viewport with params musth return a instance configured', () => {
+            const viewport = Viewport().setTransform({
+                x: 1,
+                y: 1,
+                z: 0,
+                w: 10,
+                h: 10,
+            });
+
+            expect(viewport.getTransform()).toEqual({
+                x: 1,
+                y: 1,
+                z: 0,
+                w: 10,
+                h: 10,
+            });
+        });
+    });
+
+    describe('Camera tests', () => {
+        test('Camera.getInstance must return a singleton instance from the camera', () => {
+            const cam = Camera.getInstance();
+
+            expect(cam.viewport.getTransform()).toEqual({
+                x: 0,
+                y: 0,
+                z: 0,
+                w: 0,
+                h: 0,
+            });
+        });
+
+        test('Camera must allow to set a new viewport', () => {
+            const viewport = Viewport().setTransform({
+                x: 1,
+                y: 1,
+                z: 0,
+                w: 10,
+                h: 10,
+            });
+
+            const cam = Camera.getInstance().setViewport(viewport);
+
+            expect(cam.viewport.getTransform()).toEqual({
+                x: 1,
+                y: 1,
+                z: 0,
+                w: 10,
+                h: 10,
+            });
+        });
+
+        test('Camera must be able to follow an entity centrally', () => {
+            const viewport = Viewport().setTransform({
+                x: 0,
+                y: 0,
+                w: 10,
+                h: 10,
+            });
+            const cam = Camera.getInstance().setViewport(viewport);
+            const entity = Entity().setTransform({ x: 5, y: 5, w: 5, h: 5 });
+
+            cam.followEntity(entity);
+            const { x, y } = cam.viewport.getTransform();
+
+            expect(x).toBe(2.5);
+            expect(y).toBe(2.5);
+        });
+
+        test('Camera.getPositionInViewport must return a vec3 in camera coords', () => {
+            const viewport = Viewport().setTransform({
+                x: 5,
+                y: 5,
+                z: 0,
+                w: 10,
+                h: 10,
+            });
+            const cam = Camera.getInstance().setViewport(viewport);
+            const entity = Entity().setTransform({
+                x: 2,
+                y: 2,
+                w: 10,
+                h: 10,
+            });
+
+            entity.setTransform(cam.mapTransformInViewport(entity.getTransform()));
+
+            expect(entity.getTransform()).toEqual({
+                x: -3,
+                y: -3,
+                z: 0,
+                w: 10,
+                h: 10,
+            });
+        });
+    });
+
+    describe('Resources loader tests', () => {
+        test('Resource loader factory must return a instance of resource loader', () => {
+            const loader = ResourceLoader.getInstance();
+
+            expect(loader.resources).toEqual({});
+        });
+
+        /* test('Resource loader must load the list of tuples that is passed', async () => {
+            const loader = ResourceLoader.getInstance();
+
+            await loader.load(['cat', 'some/path'], ['bear', 'some/path']);
+
+            expect(loader.get('cat')).not.toBeUndefined();
+        });*/
+    });
+});
