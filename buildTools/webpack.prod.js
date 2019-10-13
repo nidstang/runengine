3d30ef3a0ff47a568564d703659d988f70f1c03f
Pablo <pfranco@outlook.com>
2019-10-13 21:46:58 +0200

    build library config was added. Also, some dependencies were audited

--- buildTools/webpack.prod.js
+++ buildTools/webpack.prod.js
@@ -1,5 +1,19 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const base = require('./webpack.base');
 
 module.exports = merge(base, {
+    mode: 'production',
+    entry: path.resolve(__dirname, '..', 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, '..', 'dist'),
+        filename: 'runengine.umd.js',
+        library: 'runengine',
+        libraryTarget: 'umd',
+        umdNamedDefine: true,
+    },
+
+    optimization: {
+        minimize: true,
+    },
 });
